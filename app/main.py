fc23d34b83ae4b75804f6629cfd906e358d5ec5a
rahulbhatia3422 <rahulbhatia3422@gmail.com>
2026-04-04 13:52:42 +0530

    Updated code

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from app.db.database import engine   # 👈 ye add karo
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Finance Backend Running 🚀"}
+
+# 👇 YAHI add karna hai (FastAPI ke niche hi)
+@app.get("/test-db")
+def test_db():
+    try:
+        connection = engine.connect()
+        connection.close()
+        return {"message": "Database Connected Successfully ✅"}
+    except Exception as e:
+        return {"error": str(e)}
