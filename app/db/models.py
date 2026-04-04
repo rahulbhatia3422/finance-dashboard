9aa89aa6b35a29c7e322d4a318bf93b8d73e2369
rahulbhatia3422 <rahulbhatia3422@gmail.com>
2026-04-04 13:59:53 +0530

    Added database models (User, FinancialRecord tables)

---
+++ app/db/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Float, Date
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    role = Column(String) 
+    is_active = Column(String, default="active")
+
+
+class FinancialRecord(Base):
+    __tablename__ = "records"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float)
+    type = Column(String)  # income / expense
+    category = Column(String)
+    date = Column(Date)
+    notes = Column(String)
--- app/main.py
+++ app/main.py
@@ -1,18 +1,12 @@
 from fastapi import FastAPI
-from app.db.database import engine   # 👈 ye add karo
+from app.db.database import engine, Base
+from app.db import models
 
 app = FastAPI()
 
+# Tables create
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def root():
-    return {"message": "Finance Backend Running 🚀"}
-
-# 👇 YAHI add karna hai (FastAPI ke niche hi)
-@app.get("/test-db")
-def test_db():
-    try:
-        connection = engine.connect()
-        connection.close()
-        return {"message": "Database Connected Successfully ✅"}
-    except Exception as e:
-        return {"error": str(e)}
+    return {"message": "Finance Backend Running Successfully!"}
