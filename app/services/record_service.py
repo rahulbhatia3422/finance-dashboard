b256d642428ad5d7a1c2776baefd7fb3d2c4c1ac
rahulbhatia3422 <rahulbhatia3422@gmail.com>
2026-04-04 14:30:44 +0530

    Added record service for financial entries

---
+++ app/services/record_service.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Session
+from app.db import models
+
+def create_record(db: Session, record):
+    db_record = models.FinancialRecord(**record.dict())
+    db.add(db_record)
+    db.commit()
+    db.refresh(db_record)
+    return db_record
